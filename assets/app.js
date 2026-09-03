9c455e57fb4d961c77f2b3d7c06e60121a6d26eb
PustefixReisen <josef@kjwalter.de>
2026-09-03 07:11:53 +0200

    FIB-Update 03.09.2026 laden

--- assets/app.js
+++ assets/app.js
@@ -3,5 +3,6 @@
   await import('./update-20260901.js');
   await import('./update-20260901-tz.js');
   await import('./sort-topics.js');
+  await import('./update-20260903.js');
   await import('./image-features.js');
 })();
