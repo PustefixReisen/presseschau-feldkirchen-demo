2389d68fc8aee3da6bfd21e739cd76eaad0c6940
PustefixReisen <josef@kjwalter.de>
2026-09-03 15:38:11 +0200

    Redaktionelle Korrekturen vor Bildzuordnung laden

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@
   await import('./sort-topics.js');
   await import('./update-20260903.js');
   await import('./update-20260903b.js');
+  await import('./editorial-corrections-20260903.js');
   await import('./image-features.js');
   await import('./search-dynamic.js');
 })();
